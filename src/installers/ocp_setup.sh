b458c938d9eecb9b992da6f24e07982fc2b8232e
grtn316 <grtn316@hotmail.com>
2021-11-04 15:58:11 -0500

    update azurefiles configuration

--- src/installers/ocp_setup.sh
+++ src/installers/ocp_setup.sh
@@ -2,7 +2,6 @@
 
  wget -nv https://raw.githubusercontent.com/Azure/maximo/4.6/src/machinesets/db2.yaml -O /tmp/OCPInstall/db2.yaml
  wget -nv https://raw.githubusercontent.com/Azure/maximo/4.6/src/machinesets/ocs.yaml -O /tmp/OCPInstall/ocs.yaml
- #wget -nv https://raw.githubusercontent.com/Azure/maximo/4.6/src/machinesets/worker.yaml -O /tmp/OCPInstall/worker.yaml
 
 
 #Setup Zone 1
@@ -18,14 +17,6 @@
  envsubst < /tmp/OCPInstall/ocs.yaml > /tmp/OCPInstall/QuickCluster/ocs.yaml
  sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/ocs.yaml
 
-# #Setup Worker MachineSet
-#  export numReplicas=3
-#  envsubst < /tmp/OCPInstall/worker.yaml > /tmp/OCPInstall/QuickCluster/worker.yaml
-#  sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/worker.yaml
-#  sudo -E /tmp/OCPInstall/oc scale --replicas=0 machineset $(grep -A3 'name:' /tmp/OCPInstall/QuickCluster/worker.yaml | head -n1 | awk '{ print $2}') -n openshift-machine-api
-#  sudo -E /tmp/OCPInstall/oc scale --replicas=3 machineset $(grep -A3 'name:' /tmp/OCPInstall/QuickCluster/worker.yaml | head -n1 | awk '{ print $2}') -n openshift-machine-api
- 
-
  #Setup Zone 2
  export zone=2
 
@@ -39,25 +30,13 @@
  envsubst < /tmp/OCPInstall/ocs.yaml > /tmp/OCPInstall/QuickCluster/ocs.yaml
  sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/ocs.yaml
 
-#  #Setup Worker MachineSet
-#  export numReplicas=3
-#  envsubst < /tmp/OCPInstall/worker.yaml > /tmp/OCPInstall/QuickCluster/worker.yaml
-#  sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/worker.yaml
-#  sudo -E /tmp/OCPInstall/oc scale --replicas=0 machineset $(grep -A3 'name:' /tmp/OCPInstall/QuickCluster/worker.yaml | head -n1 | awk '{ print $2}') -n openshift-machine-api
-#  sudo -E /tmp/OCPInstall/oc scale --replicas=3 machineset $(grep -A3 'name:' /tmp/OCPInstall/QuickCluster/worker.yaml | head -n1 | awk '{ print $2}') -n openshift-machine-api
- 
-
-#  #Setup Zone 3
-#  export zone=3
- 
-#  #Setup Worker MachineSet
-#  export numReplicas=3
-#  envsubst < /tmp/OCPInstall/worker.yaml > /tmp/OCPInstall/QuickCluster/worker.yaml
-#  sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/worker.yaml
-#  sudo -E /tmp/OCPInstall/oc scale --replicas=0 machineset $(grep -A3 'name:' /tmp/OCPInstall/QuickCluster/worker.yaml | head -n1 | awk '{ print $2}') -n openshift-machine-api
-#  sudo -E /tmp/OCPInstall/oc scale --replicas=3 machineset $(grep -A3 'name:' /tmp/OCPInstall/QuickCluster/worker.yaml | head -n1 | awk '{ print $2}') -n openshift-machine-api
-
  #Configure Azure Files
- sudo -E /tmp/OCPInstall/oc apply -f https://raw.githubusercontent.com/Azure/maximo/4.6/src/storageclasses/azurefiles-standard.yaml
- sudo -E /tmp/OCPInstall/oc apply -f https://raw.githubusercontent.com/Azure/maximo/4.6/src/storageclasses/azurefiles-premium.yaml
+ wget -nv https://raw.githubusercontent.com/Azure/maximo/4.6/src/storageclasses/azurefiles-standard.yaml -O /tmp/OCPInstall/azurefiles-standard.yaml
+ envsubst < /tmp/OCPInstall/azurefiles-standard.yaml > /tmp/OCPInstall/QuickCluster/azurefiles-standard.yaml
+ sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/azurefiles-standard.yaml
+
+ wget -nv https://raw.githubusercontent.com/Azure/maximo/4.6/src/storageclasses/azurefiles-premium.yaml -O /tmp/OCPInstall/azurefiles-premium.yaml
+ envsubst < /tmp/OCPInstall/azurefiles-premium.yaml > /tmp/OCPInstall/QuickCluster/azurefiles-premium.yaml
+ sudo -E /tmp/OCPInstall/oc apply -f /tmp/OCPInstall/QuickCluster/azurefiles-premium.yaml
+
  sudo -E /tmp/OCPInstall/oc apply -f https://raw.githubusercontent.com/Azure/maximo/4.6/src/storageclasses/persistent-volume-binder.yaml
